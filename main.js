67ebff26b04e0a962d9b05e735ddcd4531bca95f
OPesicka <ondrejpesicka@gmail.com>
2022-11-07 07:20:39 +0100

    testing dataset and actor status message

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ import fetch from "node-fetch";
 
 await Actor.init();
 
-const { token, teamID } = await Actor.getInput();
+Actor.config;
 
-// const teamID = "912788418659599813"; // AlfaOptima
-// const token = "figd_iOtW4vr8ZiSQbSewPFzRoRcLlVr4vWL_0PrBqSWM";
+// const { token, teamID } = await Actor.getInput();
+
+const teamID = "912788418659599813"; // AlfaOptima
+const token = "figd_iOtW4vr8ZiSQbSewPFzRoRcLlVr4vWL_0PrBqSWM";
 
 function getURL(url, token) {
     return fetch(url, {
@@ -19,6 +21,7 @@ function getURL(url, token) {
 async function getProjectIds(token, teamID) {
     const url = `https://api.figma.com/v1/teams/${teamID}/projects`;
     console.log(`Getting projects from team ${teamID} `);
+    await Actor.setStatusMessage(`Getting projects from team ${teamID} `);
     const res = await getURL(url, token);
     const projects = res.projects;
     return projects;
@@ -34,6 +37,9 @@ async function getFiles(token, projectID) {
 async function getAllFiles(token, teamID) {
     const projectIds = await getProjectIds(token, teamID);
     console.log(`Getting files from ${projectIds.length} projects`);
+    await Actor.setStatusMessage(
+        `Getting files from ${projectIds.length} projects `
+    );
     const projectResponses = projectIds.map((project) =>
         getFiles(token, project.id)
     );
@@ -42,7 +48,13 @@ async function getAllFiles(token, teamID) {
 }
 
 const result = await getAllFiles(token, teamID);
+const data = { result, timestamp: new Date() };
+
+const dataset = await Actor.openDataset(`figma-file-count-${teamID}`);
+
+await dataset.pushData(data);
 
 console.log(result);
+await Actor.setStatusMessage(`Counted ${result} files `);
 
 await Actor.exit();
